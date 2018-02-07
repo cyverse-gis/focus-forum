dae60b9e26daa386249aa61bc7117f076fbadf64
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2018-02-07 09:42:48 -0700

    Update setup_grass.sh

--- atmo/setup_grass.sh
+++ atmo/setup_grass.sh
@@ -3,7 +3,7 @@
 # Setup GRASS Environment
 WORKING_DIR=/tmp
 LOCATION=$HOME/Documents/tmp_${WORKING_DIR}/PERMANENT
-GRASSRC=$HOME/.grassrc74_${WORKING_DIR}
+GRASSRC=$HOME/.grassrc72_${WORKING_DIR}
 export GISRC=${GRASSRC}
 export GRASS_VERBOSE=0
 
@@ -13,7 +13,7 @@ echo "MAPSET: PERMANENT" >> $GRASSRC
 echo "GRASS_GUI: text" >> $GRASSRC
 
 # Create a mapset
-grass74 -c epsg:3857 ${PWD}/mytmp -e
+grass72 -c epsg:3857 ${PWD}/mytmp -e
 
 # Run GRASS72
-grass74 ${PWD}/mytmp/PERMANENT
+grass72 ${PWD}/mytmp/PERMANENT
